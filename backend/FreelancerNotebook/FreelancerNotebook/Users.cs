e29cdaad0378ca10caf5cbd81ed41f64311ee152
arwojs <arwojs@gmail.com>
2021-10-30 18:27:29 +0200

    Porste modele

---
+++ backend/FreelancerNotebook/FreelancerNotebook/Users.cs
@@ -0,0 +1,11 @@
+namespace FreelancerNotebook
+{
+    public class Users
+    {
+        public int id { get; set; }
+        public string mail { get; set; }
+        public bool isAdmin { get; set; }
+        public string username { get; set; }
+        public string password { get; set; }
+    }
+}
