71640e556f0b33e0d232c8dc0a9c8b1136947621
istolesweetroll <77072731+istolesweetroll@users.noreply.github.com>
2022-01-09 15:25:52 +0100

    Worked on sign in form

--- frontend/src/app/signup/signup.component.ts
+++ frontend/src/app/signup/signup.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { UsersService } from '../services/users.service';
+import { FormControl, FormGroupDirective, NgForm, Validators, FormGroup, FormBuilder } from '@angular/forms';
+import { ErrorStateMatcher } from '@angular/material/core';
 
 interface RegisterData {
   username: string;
@@ -8,22 +10,41 @@ interface RegisterData {
   email: string;
 }
 
+export class MyErrorStateMatcher implements ErrorStateMatcher {
+  isErrorState(control: FormControl | null, form: FormGroupDirective | NgForm | null): boolean {
+    const invalidCtrl = !!(control && control.parent && control.parent.invalid && control.parent.dirty);
+    const invalidParent = !!(control && control.parent && control.parent.invalid && control.parent.dirty);
+
+    return (invalidCtrl || invalidParent);
+  }
+}
+
+
 @Component({
   selector: 'app-signup',
   templateUrl: './signup.component.html',
   styleUrls: ['./signup.component.css'],
 })
-export class SignupComponent implements OnInit {
+export class SignupComponent{
   user: RegisterData;
   w = window.innerWidth;
   disableClose: boolean;
-  constructor(private usersService: UsersService, private router: Router) {
+  myForm: FormGroup;
+  matcher = new MyErrorStateMatcher();
+  constructor(private usersService: UsersService, private router: Router, private formBuilder: FormBuilder) {
+
+    this.myForm = this.formBuilder.group({
+      password: ['', [Validators.required]],
+      confirmPassword: ['']
+    }, { validator: this.checkPasswords });
+
     this.user = {} as RegisterData;
     if (this.w > 450) {
       this.disableClose = true;
     } else {
       this.disableClose = false;
     }
+
   }
 
   registerUser(): void {
@@ -33,5 +54,12 @@ export class SignupComponent implements OnInit {
       }
     });
   }
-  ngOnInit(): void {}
+
+
+  checkPasswords(group: FormGroup) {
+    let pass = group.controls.password.value;
+    let confirmPass = group.controls.confirmPassword.value;
+
+    return pass === confirmPass ? null : { notSame: true }
+  }
 }
